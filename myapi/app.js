4498161f7a03a1da8e8d5e3f1e387cd4aeb41b02
Andreas <andrey.awetisyan@yandex.ru>
2021-02-19 14:32:40 +0200

    Add carowner folder to routes and carowner_model to models

--- myapi/app.js
+++ myapi/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var carRouter = require('./routes/car');
 var ownerRouter = require('./routes/owner');
+var carownerRouter = require('./routes/carowner');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/car', carRouter);
 app.use('/owner', ownerRouter);
+app.use('/carowner', carownerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
